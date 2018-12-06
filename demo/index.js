25bed68d0e2e796a4842651fdb9f82318b9008aa
Martin Maroši <marvusm.mmi@gmail.com>
2018-12-06 15:08:03 +0100

    Fixed wizard demo buttons.

--- demo/index.js
+++ demo/index.js
@@ -5,7 +5,7 @@ import FormRenderer from '@data-driven-forms/react-form-renderer';
 import miqSchema from './demo-schemas/miq-schema';
 import { uiArraySchema, arraySchema, schema, uiSchema, conditionalSchema } from './demo-schemas/widget-schema';
 import { formFieldsMapper, layoutMapper } from '../src';
-import { Title, Button, Toolbar, ToolbarGroup, ToolbarItem } from '@patternfly/react-core';
+import { Title, Button, Toolbar, ToolbarGroup } from '@patternfly/react-core';
 import { wizardSchema } from './demo-schemas/wizard-schema';
 import sandboxSchema from './demo-schemas/sandbox';
 
@@ -13,7 +13,7 @@ const Summary = props => <div>Custom summary component.</div>;
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { schema: wizardSchema, schemaString: 'default', sandbox: false };
+        this.state = { schema: wizardSchema, schemaString: 'default', additionalOptions: { showFormControls: false }};
     }
 
     render() {
@@ -22,36 +22,37 @@ class App extends React.Component {
             <Title size="4xl">Pf4 component mapper</Title>
             <Toolbar style={{ marginBottom: 20, marginTop: 20 }}>
                 <ToolbarGroup>
-                    <Button onClick={() => this.setState(state => ({ schema: wizardSchema, schemaString: 'default' }))}>Wizard</Button>
+                    <Button onClick={() => this.setState(state => ({ schema: wizardSchema, schemaString: 'default', additionalOptions: { showFormControls: false } }))}>Wizard</Button>
                 </ToolbarGroup>
                 <ToolbarGroup>
-                    <Button onClick={() => this.setState(state => ({ schema: arraySchema, schemaString: 'mozilla', ui: uiArraySchema}))}>ArraySchema</Button>
+                    <Button onClick={() => this.setState(state => ({ schema: arraySchema, schemaString: 'mozilla', ui: uiArraySchema, additionalOptions: {}}))}>arraySchema</Button>
                 </ToolbarGroup>
                 <ToolbarGroup>
-                    <Button onClick={() => this.setState(state => ({ schema: schema, schemaString: 'mozilla', ui: uiSchema}))}>Schema</Button>
+                    <Button onClick={() => this.setState(state => ({ schema: schema, schemaString: 'mozilla', ui: uiSchema, additionalOptions: {}}))}>schema</Button>
                 </ToolbarGroup>
                 <ToolbarGroup>
-                    <Button onClick={() => this.setState(state => ({ schema: miqSchema, schemaString: 'miq'}))}>MIQ</Button>
+                    <Button onClick={() => this.setState(state => ({ schema: miqSchema, schemaString: 'miq', additionalOptions: {}}))}>miq</Button>
                 </ToolbarGroup>
                 <ToolbarGroup>
-                    <Button onClick={() => this.setState(state => ({ schema: conditionalSchema, schemaString: 'mozilla', ui: uiSchema}))}>Conditional</Button>
+                    <Button onClick={() => this.setState(state => ({ schema: conditionalSchema, schemaString: 'mozilla', ui: uiSchema, additionalOptions: {}}))}>conditional</Button>
                 </ToolbarGroup>
                 <ToolbarGroup>
-                    <Button onClick={() => this.setState(state => ({ schema: sandboxSchema, schemaString: 'default', ui: uiSchema }))}>Sandbox</Button>
+                    <Button onClick={() => this.setState(state => ({ schema: sandboxSchema, schemaString: 'default', additionalOptions: {}}))}>Sandbox</Button>
                 </ToolbarGroup>
             </Toolbar>
-                <FormRenderer
-                    onSubmit={console.log}
-                    schemaType={this.state.schemaString && this.state.schemaString}
-                    formFieldsMapper={{
-                        ...formFieldsMapper,
-                        summary: Summary
-                    }}
-                    onCancel={() => console.log('Cancel action')}
-                    layoutMapper={layoutMapper}
-                    schema={this.state.schema}
-                    uiSchema={this.state.ui}
-                />
+            <FormRenderer
+                onSubmit={console.log}
+                schemaType={this.state.schemaString}
+                formFieldsMapper={{
+                    ...formFieldsMapper,
+                    summary: Summary
+                }}
+                onCancel={() => console.log('Cancel action')}
+                layoutMapper={layoutMapper}
+                schema={this.state.schema}
+                uiSchema={this.state.ui}
+                {...this.state.additionalOptions}
+            />
         </div>
     </div>)
     };
